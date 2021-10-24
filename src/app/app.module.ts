90cac2968ea75404e4b83c7d9cbeb4945554962c
kimutaiamos <amos.kimutai@student.moringaschool.com>
2021-10-24 13:56:31 +0300

    added class and home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -8,7 +9,13 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { UserComponent } from './user/user.component';
 import { TimeAgoPipe } from './time-ago.pipe';
 import { UserDirective } from './user.directive';
-
+import { Routes } from '@angular/router';
+import { ProfileService } from './profile.service';
+const routes: Routes = [
+  {path: 'user', component: UserComponent},
+  {path: '', redirectTo: '/user', pathMatch: 'full'},
+  {path: '**', component: NotFoundComponent}
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,9 +27,11 @@ import { UserDirective } from './user.directive';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ProfileService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
