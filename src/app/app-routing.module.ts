e53cbd21e007b069434c761afa9ffd9bff912424
kimutaiamos <amos.kimutai@student.moringaschool.com>
2021-10-24 14:09:59 +0300

    added changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { CommonModule } from '@angular/common';
+import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+//defining routes
+const routes: Routes = [
+  {path: 'user', component: UserComponent},
+  {path: '', redirectTo: '/goals', pathMatch: 'full'},
+  {path: 'user/:id', component: UserComponent},
+  {path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes),
+    CommonModule
+  ],
+  exports: [RouterModule],
+  declarations: []
 })
 export class AppRoutingModule { }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfileService } from '../profile.service';
-
+import { Users } from '../users';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers:[ProfileService]
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+   user: Users;
+  constructor(private profileservice:ProfileService) { }
 
   ngOnInit(): void {
   }
